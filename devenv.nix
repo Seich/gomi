71f956fac315cea92cd1be5b69a7086a410de9da
David Diaz <seich@martianwabbit.com>
2024-08-07 00:45:43 +0200

    Updated environment.

--- devenv.nix
+++ devenv.nix
@@ -4,15 +4,8 @@
 {
   packages = [
     pkgs.vscode-langservers-extracted
-    pkgs.nodePackages.typescript-language-server
     pkgs.prettierd
-    pkgs.caddy
-    pkgs.bun
   ];
-
-  languages.deno = {
-    enable = true;
-  };
 
   scripts.build.exec = "deno compile --allow-read --allow-write --allow-env --unstable-temporal --allow-net ./gomi.ts --output gomi";
 
