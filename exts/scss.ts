2bceb085d9e1a23d93c9a22537af5e03dfd923c7
David Diaz <seich@martianwabbit.com>
2024-08-16 01:34:18 +0200

    The sass compiler fails if given an empty file.

--- exts/scss.ts
+++ exts/scss.ts
@@ -1,6 +1,8 @@
 import sass from "https://deno.land/x/denosass@1.0.6/mod.ts";
 
 export const renderScss = (content: string) => {
+  if (!content) return "";
+
   const compiler = sass(content);
   const output = compiler.to_string("expanded") as string;
   return output;
